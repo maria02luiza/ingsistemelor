af77bb2ff4c51f3dcd86785536d96e1edff90165
Cojanu Maria-Luiza <marialuiza.cojanu@ulbsibiu.ro>
2025-03-21 11:00:38 +0200

    message

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -4,9 +4,11 @@ import ro.ulbs.paradigme.lab2.Form;
 import ro.ulbs.paradigme.lab2.simplechained.SimpleChainedList;
 import ro.ulbs.paradigme.lab2.doublechained.DoubleChainedList;
 
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) {
+    /*public static <PasswordMMaker> void main(String[] args) {
                 SimpleChainedList simpleList = new SimpleChainedList();
                 for (int i = 1; i <= 10; i++) {
                     simpleList.addValue(i);
@@ -53,12 +55,65 @@ public class Main {
              Form form4 = new Form();
              System.out.println("Numarul de instante create: " + Form.getCounter() );
              //3.6.2-calasa passwordmaker
-        PasswordMMaker passwordMaker=new PasswordMMaker("UserNme ");
-        System.out.println("Parola generata: " + passwordMaker.getPassword());
-        PasswordMMaker passwordMaker = new PasswordMMaker("UserName");
-        System.out.println("Parola generata: " + passwordMaker.getPassword("UserName"));
-    }
+        // Metoda main pentru testare
 
+            //PasswordMMaker passwordMaker = new PasswordMMaker("UserName");
+           // String password = passwordMaker.getPassword("UserName");
+           // System.out.println("Parola generată: " + password);
+*/
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        List<Integer> x = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            x.add(rand.nextInt(11));
+        }
+
+        List<Integer> y = new ArrayList();
+        for(int i=0;i<7;i++) {
+            y.add(rand.nextInt(11));
+        }
+        Collections.sort(x);
+        Collections.sort(y);
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+
+        //a
+
+        List<Integer> xPlusY = new ArrayList<>(); //a
+        xPlusY.addAll(x);
+        xPlusY.addAll(y);
+        Collections.sort(xPlusY);
+
+        //b
+
+        Set<Integer> zSet = new TreeSet<>(x); //b
+        zSet.retainAll(y);
+        List<Integer> zSetList = new ArrayList<>(zSet);
+        Collections.sort(zSetList);
+
+        //c
+
+        Set<Integer> xSet = new TreeSet<>(x);
+        Set<Integer> ySet = new TreeSet<>(y);
+        xSet.removeAll(ySet);
+        List<Integer> xMinusY = new ArrayList<>(xSet);
+        Collections.sort(xMinusY);
+
+        //d
+        int  P = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList<>();
+        for (int nr : xPlusY) {
+            if (nr <= P) {
+                xPlusYLimitedByP.add(nr);
+            }
+        }
+
+        System.out.println("xPlusY: " + xPlusY);
+        System.out.println("zSet: " + zSetList);
+        System.out.println("xMinusY: " + xMinusY);
+        System.out.println("xPlusYLimitedByP: " + xPlusYLimitedByP);
     }
 
 }
